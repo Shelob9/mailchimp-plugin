da5b4a56743d9576f7b1a51f951b7cee9610cbaf
Josh Pollock <josh@calderawp.com>
2019-05-04 17:41:58 -0400

    remove enzyme tests

--- src/blocks/block-a/components/block-components.test.js
+++ src/blocks/block-a/components/block-components.test.js
@@ -1,7 +1,5 @@
 import renderer from 'react-test-renderer';
-import {mount} from 'enzyme';
 import {DisplayA} from "./DisplayA";
-import {EditA} from "./EditA";
 
 describe('Tests working', () => {
 	it('should work', () => {
@@ -26,39 +24,4 @@ describe('DisplayA component', () => {
 });
 
 
-describe('EditA component', () => {
-	let onChangeMessage;
-	let onChangeBold;
-	beforeEach(() => {
-		onChangeMessage = jest.fn();
-		onChangeBold = jest.fn();
-	});
-	it( 'Updates message', () => {
-		const component = mount(
-			<EditA
-				onChangeMessage={onChangeMessage}
-				onChangeBold={onChangeBold}
-				useBold={true}
-				message={'Hello'}
-			/>
-		);
-		component.find( '.edit-a-message' ).find( 'input' ).simulate( 'change' );
-		expect( onChangeMessage.mock.calls.length ).toBe(1);
-		expect( onChangeBold.mock.calls.length ).toBe(0);
-	});
 
-	it( 'Updates use bold setting', () => {
-		const component = mount(
-			<EditA
-				onChangeMessage={onChangeMessage}
-				onChangeBold={onChangeBold}
-				useBold={false}
-				message={'Hello'}
-			/>
-		);
-		component.find( '.edit-a-use-bold' ).find( 'input' ).first().simulate('change', { target: { checked: true } });
-		expect( onChangeBold.mock.calls.length ).toBe(1);
-		expect( onChangeMessage.mock.calls.length ).toBe(0);
-	});
-
-});
